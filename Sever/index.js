3bc96214a3b397d8f675fb63d136d3bbfcd27929
Pengu <20521520@gm.uit.edu.vn>
2024-06-16 22:30:13 +0700

    fix wrong link api

--- Sever/index.js
+++ Sever/index.js
@@ -5,7 +5,7 @@ import { adminRouter } from "./Routes/AdminRoute.js";
 const app = express()
 app.use(cors())
 app.use(express.json())
-app.use('./auth', adminRouter)
+app.use('/auth', adminRouter)
 
 app.listen(3000, ()=> {
     console.log("Server is running")
